50d7c8dc5bb7f9ea739ad0af447d9cfb86813bb8
saeed <saeedasadsh@gmail.com>
2018-10-14 09:28:11 +0330

    123

---
+++ freeKickRepo/freeKickRepo/app.js
@@ -0,0 +1,13 @@
+﻿import * as http from "http";
+import * as express from "express";
+import * as ws from "ws";
+import { Server } from "colyseus";
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({
+    // your custom WebSocket.Server setup.
+});
+wss.listen(3015);
+const gameServer = new Server();
+gameServer.attach({ ws: wss });
