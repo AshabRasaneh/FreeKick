da3b96280fe217707c6a7eda1b9112b7b0a2559c
saeed <saeedasadsh@gmail.com>
2018-10-14 09:52:31 +0330

    123

--- freeKickRepo/freeKickRepo/app.js
+++ freeKickRepo/freeKickRepo/app.js
@@ -4,4 +4,4 @@ const http = require("http");
 const gameServer = new colyseus.Server({
     server: http.createServer()
 });
-gameServer.listen(3020, 188.253.2.147);
+gameServer.listen(3020,"188.253.2.147");
