7e9879f6cdddf8aafd05ee68fea1fe0cf82902c3
saeed <saeedasadsh@gmail.com>
2018-10-14 12:43:51 +0330

    123

--- freeKickRepo/freeKickRepo/server.js
+++ freeKickRepo/freeKickRepo/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 const colyseus = require("colyseus");
-const ChatRoom = require('./chat_room');
+const ChatRoom = require('r./rooms/chat_room.js');
 
 const PORT = process.env.PORT || 3015;
 
