95d0fc2c520ccfd1cf472a61d97c2397f541fe6a
saeed <saeedasadsh@gmail.com>
2018-10-15 12:01:02 +0330

    123

--- freeKickRepo/freeKickRepo/server.js
+++ freeKickRepo/freeKickRepo/server.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
+var express_1 = require("express");
 var http_1 = require("http");
 var colyseus_1 = require("colyseus");
 var fr1_1 = require("./rooms/fr1");
 var port = Number(process.env.PORT || 3015);
-var app = express();
+var app = express_1.express();
 var gameServer = new colyseus_1.Server({
     server: http_1.createServer(app)
 });
