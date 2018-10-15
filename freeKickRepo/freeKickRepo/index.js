eb6bda90e05fad03b901ef2b5f8a96c65b825e56
saeed <saeedasadsh@gmail.com>
2018-10-15 14:31:56 +0330

    123

---
+++ freeKickRepo/freeKickRepo/index.js
@@ -0,0 +1,25 @@
+﻿const http = require("http");
+const express = require("express");
+
+const colyseus = require("colyseus");
+const ChatRoom = require('./chat_room');
+
+const PORT = process.env.PORT || 3015;
+
+const app = new express();
+const gameServer = new colyseus.Server({
+    server: http.createServer(app)
+});
+
+// Register ChatRoom as "chat"
+gameServer.register("chat", ChatRoom);
+
+app.get("/something", function (req, res) {
+    console.log("something!", process.pid);
+    res.send("Hey!");
+});
+
+// Listen on specified PORT number
+gameServer.listen(PORT);
+
+console.log("Running on ws://localhost:" + PORT);
--- freeKickRepo/freeKickRepo/rooms/fr1.js
+++ freeKickRepo/freeKickRepo/rooms/fr1.js
@@ -2,17 +2,22 @@
 
 class fr1 extends Room {
 
+var playerReady = 0;
+
 constructor() {
     super();
     this.maxClients = 2;
     this.setState({
-        players: { }
+        players: { },
+        shootData:"",
+        fk:"",
+        diveData:""
     });
   }
 
 onInit(options) {
-    //this.setPatchRate(1000 / 20);
-    //this.setSimulationInterval(this.update.bind(this));
+    this.setPatchRate(1000 / 20);
+    this.setSimulationInterval(this.update.bind(this));
     
     console.log("ChatRoom created!", options);
 }
@@ -35,17 +40,33 @@ onLeave(client) {
 onMessage(client, data) {
     console.log(data, "received from", client.sessionId);
     //this.state.messages.push(client.sessionId + " sent " + data);
-    this.state.players[client.sessionId] = { style: data };
+    var tp = data.type;
+    if (tp == "style") {
+        this.state.players[client.sessionId] = { style: data, isFk: playerReady, ShootData: "", };
+        this.broadcast({ type: "style", data: data });
+    }
+    else if (tp == "ready") {
+        playerReady++;
+        if (playerReady == 2) {
+            this.broadcast({ type: "startGame", data:"1"});
+        }
+    }
     
-    this.broadcast(data);
+    
 }
 
-//update() {
-//    console.log("num clients:", Object.keys(this.clients).length);
-//    for (var sessionId in this.state.players) {
-//        this.state.players[sessionId].x += 0.0001;
-//    }
-//}
+update() {
+
+    if (Object.keys(this.clients).length == 2) {
+        this.broadcast({type:"style"});
+    }
+    
+    //console.log("num clients:", Object.keys(this.clients).length);
+    //for (var sessionId in this.state.players) {
+    //    this.state.players[sessionId].x += 0.0001;
+    //}
+
+}
 
 onDispose() {
     console.log("Dispose ChatRoom");
