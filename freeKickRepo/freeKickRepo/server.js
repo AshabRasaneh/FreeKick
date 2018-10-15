215fce1720b6edffad6e11fe29ed653927f0b9c8
saeed <saeedasadsh@gmail.com>
2018-10-15 11:52:16 +0330

    123

--- freeKickRepo/freeKickRepo/server.js
+++ freeKickRepo/freeKickRepo/server.js
@@ -1,17 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var http_1 = require("http");
-var colyseus_1 = require("colyseus");
-var fr1_1 = require("./rooms/fr1");
-var port = Number(process.env.PORT || 3015);
-var app = express();
-var gameServer = new colyseus_1.Server({
-    server: http_1.createServer(app)
-});
-gameServer.register("fr1", fr1_1.fr1);
-gameServer.onShutdown(function () {
-    console.log("game server is going down.");
-});
-gameServer.listen(port);
+//import * as express from 'express';
+//import { createServer } from "http";
+//import { Server } from "colyseus";
+//import { fr1 } from "./rooms/fr1";
+//const port = Number(process.env.PORT || 3015);
+//const app = express();
+//const gameServer = new Server({
+//    server: createServer(app)
+//});
+//gameServer.register("fr1", fr1);
+//gameServer.onShutdown(function () {
+//    console.log(`game server is going down.`);
+//});
+//gameServer.listen(port);
+console.log("hi");
 //# sourceMappingURL=server.js.map
