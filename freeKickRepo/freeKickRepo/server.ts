c115014f92cfcdb7b47131720063801b03c05660
saeed <saeedasadsh@gmail.com>
2018-10-15 11:59:48 +0330

    123

--- freeKickRepo/freeKickRepo/server.ts
+++ freeKickRepo/freeKickRepo/server.ts
@@ -1,25 +1,23 @@
-﻿//import * as express from 'express';
-//import { createServer } from "http";
-//import { Server } from "colyseus";
+﻿import * as express from 'express';
+import { createServer } from "http";
+import { Server } from "colyseus";
 
 
 
-//import { fr1 } from "./rooms/fr1";
+import { fr1 } from "./rooms/fr1";
 
-//const port = Number(process.env.PORT || 3015);
-//const app = express();
+const port = Number(process.env.PORT || 3015);
+const app = express();
 
-//const gameServer = new Server({
-//    server: createServer(app)
-//});
+const gameServer = new Server({
+    server: createServer(app)
+});
 
-//gameServer.register("fr1", fr1);
+gameServer.register("fr1", fr1);
 
-//gameServer.onShutdown(function () {
-//    console.log(`game server is going down.`);
-//});
+gameServer.onShutdown(function () {
+    console.log(`game server is going down.`);
+});
 
-//gameServer.listen(port);
-
-console.log("hi");
+gameServer.listen(port);
 
