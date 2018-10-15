c01d2f2402afa6345e6f7eb3f87cf1725451e5b2
saeed <saeedasadsh@gmail.com>
2018-10-15 12:10:04 +0330

    123

--- freeKickRepo/freeKickRepo/rooms/fr1.js
+++ freeKickRepo/freeKickRepo/rooms/fr1.js
@@ -1,71 +1,55 @@
-"use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    }
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-var colyseus_1 = require("colyseus");
-var fr1 = /** @class */ (function (_super) {
-    __extends(fr1, _super);
-    function fr1() {
-        var _this = _super !== null && _super.apply(this, arguments) || this;
-        _this.maxClients = 2;
-        return _this;
-        //update() {
-        //    console.log("num clients:", Object.keys(this.clients).length);
-        //    //for (var sessionId in this.state.players) {
-        //    //    this.state.players[sessionId].x += 0.0001;
-        //    //}
-        //}
-    }
-    // Authorize client based on provided options before WebSocket handshake is complete
-    fr1.prototype.onAuth = function (options) {
-        return true;
-    };
-    // When room is initialized
-    fr1.prototype.onInit = function (options) {
-        //this.setState({
-        //    players: {}
-        //});
-        //this.setPatchRate(1000 / 20);
-        //this.setSimulationInterval(this.update.bind(this));
-        console.log("fr1 created!", options);
-    };
-    // Checks if a new client is allowed to join. (default: `return true`)
-    fr1.prototype.requestJoin = function (options, isNew) {
-        return true;
-    };
-    // When client successfully join the room
-    fr1.prototype.onJoin = function (client) {
-        this.broadcast(client.sessionId + " joined.");
-        //this.state.players[client.sessionId] = {specification:""}
-    };
-    // When a client leaves the room
-    fr1.prototype.onLeave = function (client, consented) {
-        this.broadcast(client.sessionId + " left.");
-    };
-    // When a client sends a message
-    fr1.prototype.onMessage = function (client, message) {
-        console.log("fr1 received message from", client.sessionId, ":", message);
-        this.broadcast("(" + client.sessionId + ") " + message.message);
-        //if (message.action === "setSpec") {
-        //    this.state.players[client.sessionId].specification = message;
-        //}
-    };
-    // Cleanup callback, called after there are no more clients in the room. (see `autoDispose`)
-    fr1.prototype.onDispose = function () {
-        console.log("Dispose fr1");
-    };
-    return fr1;
-}(colyseus_1.Room));
-exports.fr1 = fr1;
-//# sourceMappingURL=fr1.js.map
+﻿var Room = require('colyseus').Room;
+
+class fr1 extends Room {
+
+constructor() {
+    super();
+    maxClients = 2;
+    //this.setState({
+    //    players: { }
+    //});
+  }
+
+onInit(options) {
+    //this.setPatchRate(1000 / 20);
+    //this.setSimulationInterval(this.update.bind(this));
+    
+    console.log("ChatRoom created!", options);
+}
+
+requestJoin(options) {
+    console.log("request join!", options);
+    return true;
+}
+
+onJoin(client) {
+    console.log("client joined!", client.sessionId);
+    //this.state.players[client.sessionId] = { x: 0, y: 0 };
+}
+
+onLeave(client) {
+    console.log("client left!", client.sessionId);
+    //delete this.state.players[client.sessionId];
+}
+
+onMessage(client, data) {
+    console.log(data, "received from", client.sessionId);
+    //this.state.messages.push(client.sessionId + " sent " + data);
+    
+    this.broadcast({ hello: "hello world" });
+}
+
+//update() {
+//    console.log("num clients:", Object.keys(this.clients).length);
+//    for (var sessionId in this.state.players) {
+//        this.state.players[sessionId].x += 0.0001;
+//    }
+//}
+
+onDispose() {
+    console.log("Dispose ChatRoom");
+}
+
+}
+
+module.exports = fr1;
--- freeKickRepo/freeKickRepo/server.js
+++ freeKickRepo/freeKickRepo/server.js
@@ -1,17 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var http_1 = require("http");
-var colyseus_1 = require("colyseus");
-var fr1_1 = require("./rooms/fr1");
-var port = Number(process.env.PORT || 3015);
-var app = express_1.express();
-var gameServer = new colyseus_1.Server({
-    server: http_1.createServer(app)
+﻿const http = require("http");
+const express = require("express");
+
+const colyseus = require("colyseus");
+const frone = require('./rooms/fr1');
+
+const PORT = process.env.PORT || 3015;
+
+const app = new express();
+const gameServer = new colyseus.Server({
+    server: http.createServer(app)
 });
-gameServer.register("fr1", fr1_1.fr1);
-gameServer.onShutdown(function () {
-    console.log("game server is going down.");
+
+// Register ChatRoom as "chat"
+gameServer.register("fr1", frone);
+
+app.get("/something", function (req, res) {
+    console.log("something!", process.pid);
+    res.send("Hey!");
 });
-gameServer.listen(port);
-//# sourceMappingURL=server.js.map
+
+// Listen on specified PORT number
+gameServer.listen(PORT);
+
+console.log("Running on ws://localhost:" + PORT);
